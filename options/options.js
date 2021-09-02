755aad2dfec353329460f667bf4e88e4e5a27c4e
Crowdbotics <team@crowdbotics.com>
2021-09-02 03:40:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "frosty_paper_30265Identifier",
+  url: "https://frosty_paper_30265Identifier.botics.co",
+  api: "https://frosty_paper_30265Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
